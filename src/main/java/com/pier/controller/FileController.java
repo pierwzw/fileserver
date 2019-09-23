22cef810220d650a4fdf342784fea2e750433e4f
zhongweiwu <zhongweiwu@pptv.com>
2019-09-23 10:56:50 +0800

    modify

--- src/main/java/com/pier/controller/FileController.java
+++ src/main/java/com/pier/controller/FileController.java
@@ -1,17 +1,6 @@
 package com.pier.controller;
 
-import java.io.*;
-import java.net.URLEncoder;
-import java.util.*;
-
-import javax.print.attribute.standard.Media;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.alibaba.fastjson.JSON;
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,16 +8,26 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.System.out;
 
 /**
+ * 必须在server.xml中设置utf-8转码
  * @auther zhongweiwu
  * @date 2019/3/21 11:56
  */
